d392ddb5672a48c0328da556844beca86865653d
TrilochanprasadBH <tptrilochanprasad@gmail.com>
2025-05-19 22:03:37 +0530

    some txt notes and js

--- L2P1-Accordion/index.js
+++ L2P1-Accordion/index.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-        // const accordionHeaders = document.querySelectorAll('.accordion-header');
+    //get hold of all accordion headers
+        const accordionHeaders = document.querySelectorAll('.accordion-header');
+        //add event listener to all accordion headers, forEach one of them.
+        accordionHeaders.forEach(header => {
+            //each header is a different one, so dynamically catch hold of header-1,2,3,etc like this
+            header.addEventListener('click', ()=>{
+                //this here refers to the clicked header, among header that we are mapping.
+                const ContentPanelId = this.getAttribute('aria-controls');
+                const contentPanel = document.getElementById(ContentPanelId);
 
+
+            })
+        })
 })
