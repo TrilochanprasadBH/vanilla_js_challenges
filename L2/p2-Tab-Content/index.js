84a515e26af469efdccf95aebb1fbfa94dcdc4ce
TrilochanprasadBH <tptrilochanprasad@gmail.com>
2025-05-22 12:39:46 +0530

    tab content core working

--- L2/p2-Tab-Content/index.js
+++ L2/p2-Tab-Content/index.js
@@ -1,12 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-        //get hold of all buttons/tabs
-        const allTabs = document.querySelectorAll('.tab-button');
-        allTabs.forEach(tab => {
-            tab.addEventListener('click', function () {
-                const tabContentId = this.getAttribute('aria-controls');
-                const tabContent = document.getElementById(tabContentId);
+    //get hold of all buttons/tabs
+    const allTabs = document.querySelectorAll('.tab-button');
+    allTabs.forEach(tab => {
+        // console.log(tab);
+        tab.addEventListener('click', function () {
+            const tabContentId = this.getAttribute('aria-controls');
+            const tabContent = document.getElementById(tabContentId);
 
+            const isTabContentCurrentlyNotActive = tabContent.classList.contains('is-hidden');
+
+            allTabs.forEach(otherTab => {
+                if(otherTab !== tab){
+                    const otherTabContentId = otherTab.getAttribute('aria-controls');
+                    const otherTabContent = document.getElementById(otherTabContentId);
+                    otherTabContent.classList.add('is-hidden');
+                }
 
             })
+
+            if(isTabContentCurrentlyNotActive){
+                tabContent.classList.remove('is-hidden');
+            }
+
         })
+    })
 })
