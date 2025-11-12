6353f505abd0c8a55ab40eb94f53203e4c540835
Trilochanprasad B Hilli <tptrilochanprasad@gmail.com>
2025-11-12 23:08:14 +0530

    Create 2.js

---
+++ core-js-challenges/2.js
@@ -0,0 +1,23 @@
+// 2.Add async pieces (setTimeout, Promise) to show how the event loop re-enters the stack.
+
+console.log("Start");
+
+//settime out outer and promise  inner to demonstrate event loop behavior
+setTimeout(() => {
+  console.log("Timeout 1");
+
+  Promise.resolve().then(() => {
+    console.log("Promise 1");
+  });
+}, 0);
+
+// promise outer and settime out inner to demonstrate event loop behavior
+Promise.resolve().then(() => {
+  console.log("Promise 2");
+
+  setTimeout(() => {
+    console.log("Timeout 2");
+  }, 0);
+});
+
+console.log("End");
